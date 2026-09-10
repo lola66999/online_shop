6ed19da12a483e38234c425f8b6109971c3c7df9
lola <xayrullayev001@gmail.com>
2026-09-10 15:13:01 +0500

    customer section

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,4 +1,7 @@
 import { CirclePlus, MoreVertical } from "lucide-react";
+import React, { useState } from "react";
+import { Table } from "antd";
+
 import img1 from "../assets/img/product1.png";
 import img2 from "../assets/img/product2.png";
 import img3 from "../assets/img/product3.png";
@@ -8,7 +11,60 @@ import img6 from "../assets/img/product6.png";
 import img7 from "../assets/img/product7.png";
 import img8 from "../assets/img/product8.png";
 
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+  },
+];
+const dataSource = Array.from({ length: 46 }).map((_, i) => ({
+  key: i,
+  name: `Edward King ${i}`,
+  age: 32,
+  address: `London, Park Lane no. ${i}`,
+}));
+
 export default function Categories() {
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const onSelectChange = (newSelectedRowKeys) => {
+    console.log("selectedRowKeys changed: ", newSelectedRowKeys);
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+    selections: [
+      Table.SELECTION_ALL,
+      Table.SELECTION_INVERT,
+      Table.SELECTION_NONE,
+      {
+        key: "odd",
+        text: "Select Odd Row",
+        onSelect: (changeableRowKeys) => {
+          setSelectedRowKeys(
+            changeableRowKeys.filter((_, index) => index % 2 === 0),
+          );
+        },
+      },
+      {
+        key: "even",
+        text: "Select Even Row",
+        onSelect: (changeableRowKeys) => {
+          setSelectedRowKeys(
+            changeableRowKeys.filter((_, index) => index % 2 !== 0),
+          );
+        },
+      },
+    ],
+  };
   return (
     <div>
       <div className="mt-5 px-2 py-2">
@@ -29,37 +85,92 @@ export default function Categories() {
         </div>
         <div className="mt-5 grid grid-cols-4 gap-y-3">
           <div className="w-60.5 h-22 rounded-md bg-white p-3 gap-2 flex place-items-center shadow-[0px_1px_3px_0px_#00000033]">
-            <img src={img1} alt="rasm" className="w-16 h-16 rotate-0 opacity-100 rounded border border-gray-300" />
-            <p className="font-['Lato'] font-medium text-lg leading-[100%] tracking-[0.5%] text-black">Electronics</p>
+            <img
+              src={img1}
+              alt="rasm"
+              className="w-16 h-16 rotate-0 opacity-100 rounded border border-gray-300"
+            />
+            <p className="font-['Lato'] font-medium text-lg leading-[100%] tracking-[0.5%] text-black">
+              Electronics
+            </p>
           </div>
-           <div className="w-60.5 h-22 rounded-md bg-white p-3 gap-2 flex place-items-center shadow-[0px_1px_3px_0px_#00000033]">
-            <img src={img2} alt="rasm" className="w-16 h-16 rotate-0 opacity-100 rounded border border-gray-300" />
-            <p className="font-['Lato'] font-medium text-lg leading-[100%] tracking-[0.5%] text-black">Fashion</p>
+          <div className="w-60.5 h-22 rounded-md bg-white p-3 gap-2 flex place-items-center shadow-[0px_1px_3px_0px_#00000033]">
+            <img
+              src={img2}
+              alt="rasm"
+              className="w-16 h-16 rotate-0 opacity-100 rounded border border-gray-300"
+            />
+            <p className="font-['Lato'] font-medium text-lg leading-[100%] tracking-[0.5%] text-black">
+              Fashion
+            </p>
           </div>
-           <div className="w-60.5 h-22 rounded-md bg-white p-3 gap-2 flex place-items-center shadow-[0px_1px_3px_0px_#00000033]">
-            <img src={img3} alt="rasm" className="w-16 h-16 rotate-0 opacity-100 rounded border border-gray-300" />
-            <p className="font-['Lato'] font-medium text-lg leading-[100%] tracking-[0.5%] text-black">Accessories</p>
+          <div className="w-60.5 h-22 rounded-md bg-white p-3 gap-2 flex place-items-center shadow-[0px_1px_3px_0px_#00000033]">
+            <img
+              src={img3}
+              alt="rasm"
+              className="w-16 h-16 rotate-0 opacity-100 rounded border border-gray-300"
+            />
+            <p className="font-['Lato'] font-medium text-lg leading-[100%] tracking-[0.5%] text-black">
+              Accessories
+            </p>
           </div>
-           <div className="w-60.5 h-22 rounded-md bg-white p-3 gap-2 flex place-items-center shadow-[0px_1px_3px_0px_#00000033]">
-            <img src={img4} alt="rasm" className="w-16 h-16 rotate-0 opacity-100 rounded border border-gray-300" />
-            <p className="font-['Lato'] font-medium text-lg leading-[100%] tracking-[0.5%] text-black">Home & Kitchen</p>
+          <div className="w-60.5 h-22 rounded-md bg-white p-3 gap-2 flex place-items-center shadow-[0px_1px_3px_0px_#00000033]">
+            <img
+              src={img4}
+              alt="rasm"
+              className="w-16 h-16 rotate-0 opacity-100 rounded border border-gray-300"
+            />
+            <p className="font-['Lato'] font-medium text-lg leading-[100%] tracking-[0.5%] text-black">
+              Home & Kitchen
+            </p>
           </div>
-           <div className="w-60.5 h-22 rounded-md bg-white p-3 gap-2 flex place-items-center shadow-[0px_1px_3px_0px_#00000033]">
-            <img src={img5} alt="rasm" className="w-16 h-16 rotate-0 opacity-100 rounded border border-gray-300" />
-            <p className="font-['Lato'] font-medium text-lg leading-[100%] tracking-[0.5%] text-black">Sports & Outdoors</p>
+          <div className="w-60.5 h-22 rounded-md bg-white p-3 gap-2 flex place-items-center shadow-[0px_1px_3px_0px_#00000033]">
+            <img
+              src={img5}
+              alt="rasm"
+              className="w-16 h-16 rotate-0 opacity-100 rounded border border-gray-300"
+            />
+            <p className="font-['Lato'] font-medium text-lg leading-[100%] tracking-[0.5%] text-black">
+              Sports & Outdoors
+            </p>
           </div>
-           <div className="w-60.5 h-22 rounded-md bg-white p-3 gap-2 flex place-items-center shadow-[0px_1px_3px_0px_#00000033]">
-            <img src={img6} alt="rasm" className="w-16 h-16 rotate-0 opacity-100 rounded border border-gray-300" />
-            <p className="font-['Lato'] font-medium text-lg leading-[100%] tracking-[0.5%] text-black">Toys & Games</p>
+          <div className="w-60.5 h-22 rounded-md bg-white p-3 gap-2 flex place-items-center shadow-[0px_1px_3px_0px_#00000033]">
+            <img
+              src={img6}
+              alt="rasm"
+              className="w-16 h-16 rotate-0 opacity-100 rounded border border-gray-300"
+            />
+            <p className="font-['Lato'] font-medium text-lg leading-[100%] tracking-[0.5%] text-black">
+              Toys & Games
+            </p>
           </div>
-           <div className="w-60.5 h-22 rounded-md bg-white p-3 gap-2 flex place-items-center shadow-[0px_1px_3px_0px_#00000033]">
-            <img src={img7} alt="rasm" className="w-16 h-16 rotate-0 opacity-100 rounded border border-gray-300" />
-            <p className="font-['Lato'] font-medium text-lg leading-[100%] tracking-[0.5%] text-black">Health & Fitness</p>
+          <div className="w-60.5 h-22 rounded-md bg-white p-3 gap-2 flex place-items-center shadow-[0px_1px_3px_0px_#00000033]">
+            <img
+              src={img7}
+              alt="rasm"
+              className="w-16 h-16 rotate-0 opacity-100 rounded border border-gray-300"
+            />
+            <p className="font-['Lato'] font-medium text-lg leading-[100%] tracking-[0.5%] text-black">
+              Health & Fitness
+            </p>
           </div>
-           <div className="w-60.5 h-22 rounded-md bg-white p-3 gap-2 flex place-items-center shadow-[0px_1px_3px_0px_#00000033]">
-            <img src={img8} alt="rasm" className="w-16 h-16 rotate-0 opacity-100 rounded border border-gray-300" />
-            <p className="font-['Lato'] font-medium text-lg leading-[100%] tracking-[0.5%] text-black">Books</p>
+          <div className="w-60.5 h-22 rounded-md bg-white p-3 gap-2 flex place-items-center shadow-[0px_1px_3px_0px_#00000033]">
+            <img
+              src={img8}
+              alt="rasm"
+              className="w-16 h-16 rotate-0 opacity-100 rounded border border-gray-300"
+            />
+            <p className="font-['Lato'] font-medium text-lg leading-[100%] tracking-[0.5%] text-black">
+              Books
+            </p>
           </div>
+        </div>
+        <div className="absolute w-285 h-236 top-108  rotate-0 opacity-100 rounded-lg bg-white shadow-[0px_1px_3px_0px_#00000033]">
+          <Table
+            rowSelection={rowSelection}
+            columns={columns}
+            dataSource={dataSource}
+          />
         </div>
       </div>
     </div>
--- src/pages/Customer.jsx
+++ src/pages/Customer.jsx
@@ -1,7 +1,75 @@
-export default function Customer(){
-    return(
-        <div>
-            <h1>Customer</h1>
+import { CirclePlus, MoreVertical, ArrowUp } from "lucide-react";
+export default function Customer() {
+  return (
+    <div>
+      <div className="grid grid-rows-3">
+        <div className="w-67.5 h-33.75 opacity-100 rounded-lg bg-white shadow-[0px_1px_3px_0px_#00000033] mt-5">
+          <div className="flex justify-between px-3 py-3">
+            <h1 className="text-[#23272E] font-lato font-bold text-[18px] leading-6.5 tracking-[0%]">
+              Total Customer
+            </h1>
+            <button className="p-1 rounded-full hover:bg-gray-100">
+              <MoreVertical size={18} className="text-gray-600" />
+            </button>
+          </div>
+          <section className="flex gap-1 place-items-center  px-3 py-3">
+            <b className="text-[#023337] font-lato font-bold text-[24px] leading-[100%]">
+              11.040
+            </b>
+            <ArrowUp size={18} className="text-[#1EB564]" />
+            <p className="font-lato font-medium text-[14px] leading-[100%] text-[#1EB564]">
+              14.4%
+            </p>
+          </section>
+          <p className="px-4 text-[#6A717F] font-lato font-normal text-[14px] leading-[100%] tracking-[-2%]">
+            Last 7 days{" "}
+          </p>
         </div>
-    )
+        <div className="w-67.5 h-33.75 opacity-100 rounded-lg bg-white shadow-[0px_1px_3px_0px_#00000033] mt-5">
+          <div className="flex justify-between px-3 py-3">
+            <h1 className="text-[#23272E] font-lato font-bold text-[18px] leading-6.5 tracking-[0%]">
+              New Customer
+            </h1>
+            <button className="p-1 rounded-full hover:bg-gray-100">
+              <MoreVertical size={18} className="text-gray-600" />
+            </button>
+          </div>
+          <section className="flex gap-1 place-items-center  px-3 py-3">
+            <b className="text-[#023337] font-lato font-bold text-[24px] leading-[100%]">
+              2.370
+            </b>
+            <ArrowUp size={18} className="text-[#1EB564]" />
+            <p className="font-lato font-medium text-[14px] leading-[100%] text-[#1EB564]">
+              14.4%
+            </p>
+          </section>
+          <p className="px-4 text-[#6A717F] font-lato font-normal text-[14px] leading-[100%] tracking-[-2%]">
+            Last 7 days{" "}
+          </p>
+        </div>
+        <div className="w-67.5 h-33.75 opacity-100 rounded-lg bg-white shadow-[0px_1px_3px_0px_#00000033] mt-5">
+          <div className="flex justify-between px-3 py-3">
+            <h1 className="text-[#23272E] font-lato font-bold text-[18px] leading-6.5 tracking-[0%]">
+              Visitor
+            </h1>
+            <button className="p-1 rounded-full hover:bg-gray-100">
+              <MoreVertical size={18} className="text-gray-600" />
+            </button>
+          </div>
+          <section className="flex gap-1 place-items-center  px-3 py-3">
+            <b className="text-[#023337] font-lato font-bold text-[24px] leading-[100%]">
+              250K``
+            </b>
+            <ArrowUp size={18} className="text-[#1EB564]" />
+            <p className="font-lato font-medium text-[14px] leading-[100%] text-[#1EB564]">
+              14.4%
+            </p>
+          </section>
+          <p className="px-4 text-[#6A717F] font-lato font-normal text-[14px] leading-[100%] tracking-[-2%]">
+            Last 7 days{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
